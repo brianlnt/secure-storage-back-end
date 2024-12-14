b274474ecf110d088572592737f77a14e9460908
Brian <thinhle1199@gmail.com>
2024-12-14 10:14:59 -0600

    init project

---
+++ src/main/java/project/brianle/securestorage/SecureStorageApplication.java
@@ -0,0 +1,13 @@
+package project.brianle.securestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/brianle/securestorage/SecureStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package project.brianle.securestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
