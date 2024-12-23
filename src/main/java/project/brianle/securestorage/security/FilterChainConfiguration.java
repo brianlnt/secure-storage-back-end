a176e261fe43b63c283e2f263868c1e09d6e0341
Brian <thinhle1199@gmail.com>
2024-12-23 13:56:08 -0600

    feat: implement FilterChainConfiguration

--- src/main/java/project/brianle/securestorage/security/FilterChainConfiguration.java
+++ src/main/java/project/brianle/securestorage/security/FilterChainConfiguration.java
@@ -3,37 +3,69 @@ package project.brianle.securestorage.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import project.brianle.securestorage.service.UserService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import project.brianle.securestorage.handler.ApiAccessDeniedHandler;
+import project.brianle.securestorage.handler.ApiAuthenticationEntryPoint;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static com.google.common.net.HttpHeaders.X_REQUESTED_WITH;
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static project.brianle.securestorage.constant.Constants.*;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class FilterChainConfiguration {
+    private final ApiAccessDeniedHandler apiAccessDeniedHandler;
+    private final ApiAuthenticationEntryPoint apiAuthenticationEntryPoint;
+    private final CustomHttpConfigurer customHttpConfigurer;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .exceptionHandling(exception ->
+                        exception.accessDeniedHandler(apiAccessDeniedHandler)
+                                .authenticationEntryPoint(apiAuthenticationEntryPoint))
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/user/login").permitAll()
+                        request.requestMatchers(PUBLIC_URLS).permitAll()
+                                .requestMatchers(OPTIONS).permitAll()
+                                .requestMatchers(DELETE, "/user/delete/**")
+                                .hasAnyAuthority("user:delete")
+                                .requestMatchers(DELETE, "/document/delete/**")
+                                .hasAnyAuthority("document:delete")
                         .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults())
-//                .formLogin(AbstractHttpConfigurer::disable)
-                .build();
+                .with(customHttpConfigurer, withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
+        corsConfiguration.setExposedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
+        corsConfiguration.setAllowedMethods(Arrays.asList(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name()));
+        corsConfiguration.setMaxAge(3600L);
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(BASE_PATH, corsConfiguration);
+        return source;
     }
 }
 
