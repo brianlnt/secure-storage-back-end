73f3d7e86abfd20dcd8970ebb5d9ed209a4dff5a
Brian <thinhle1199@gmail.com>
2024-12-23 13:29:25 -0600

    feat: implement ExceptionHandler to give consolidate exception response to users

---
+++ src/main/java/project/brianle/securestorage/handler/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package project.brianle.securestorage.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static project.brianle.securestorage.utils.RequestUtils.handleErrorResponse;
+
+@Component
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        handleErrorResponse(request, response, exception);
+    }
+}
