640d5ca8ff72fc70137195f150a15f686ec4add1
Brian <thinhle1199@gmail.com>
2024-12-23 13:42:30 -0600

    feat: implement CustomHttpConfigurer

---
+++ src/main/java/project/brianle/securestorage/security/CustomHttpConfigurer.java
@@ -0,0 +1,31 @@
+package project.brianle.securestorage.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import project.brianle.securestorage.service.JwtService;
+import project.brianle.securestorage.service.UserService;
+
+@Component
+@RequiredArgsConstructor
+public class CustomHttpConfigurer extends AbstractHttpConfigurer<CustomHttpConfigurer, HttpSecurity> {
+    private final CustomAuthorizationFilter customAuthorizationFilter;
+    private final CustomAuthenticationProvider customAuthenticationProvider;
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Override
+    public void init(HttpSecurity http) throws Exception {
+        http.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(customAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAfter(new CustomAuthenticationFilter(authenticationConfiguration.getAuthenticationManager(), userService, jwtService), UsernamePasswordAuthenticationFilter.class);
+    }
+}
